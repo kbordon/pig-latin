f124dc63246a799d8ecfc80a77e4aec65db74bf3
Alan Falcon and Kimberly Bordon <alan+kimberly@>
2017-09-20 10:43:18 -0700

    add front end and first logic in backend to address first spec in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
+// back end
+var translateToASCII = function(input) {
+  var ascii = input.charCodeAt(0);
+  if (ascii <65 || (ascii >90 && ascii <96 || ascii > 122)) {
+    alert("do nothing");
+  }
+}
+// front end
 $(document).ready(function() {
   $('form').submit(function(event) {
-    event.PreventDefault();
+    event.preventDefault();
     var input = $('input#string').val();
 
-    var result = translateInput(input);
+    var result=translateToASCII(input);
     $("#output").text(result);
     $("#output").show();
   });
